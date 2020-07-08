0e7fa58a52b45b2221b66bd0a67bd7395934133c
jcanode <45806280+jcanode@users.noreply.github.com>
2020-07-07 20:46:03 -0700

    Worked more on data package

--- small_nn/data.py
+++ small_nn/data.py
@@ -1,24 +1,76 @@
+import os
+import numpy as np
+from PIL import Image
+
 class data():
     """docstring for data."""
 
-    def __init__(self, arg):
+    def __init__(self, data, labels, type):
         super(data, self).__init__()
-        self.arg = arg
+        self.data = data
+        self.lables = labels
+        self.type = type
+
+
+def readFile(dataFile, labelFile, type):
+    dfile = open(dataFile)
+    fileData = dfile.read()
+    lfile = open(labelFile)
+    labelData = lfile.read()
+    d = data(fileData, labelData, type)
+    lfile.close()
+    dfile.close()
+    return d
+
+def preprocess(data, inputSize):
+    type = data.type
+    if (type=="image"):
+        images = []
+        images = data.data
+        for i in images:
+            images[i] = imageResize(images[i], inputSize)
+    elif(type=="text"):
+        raise NotImplementedError
+
+
+    raise NotImplementedError
+
+def imageResize(image):
+    img = Image.open(image).convert('RGBA')
+    arr = np.array(img)
+    flatArr = arr.ravel()
+    arrSize = len(flat_arr)
+    flatArr /= flatArr
+    return flatArr
+
+
+
+def readFolder(dataFolder, labelFolder, type):
+    raise NotImplementedError
+
+def imageParse(data):
+    raise NotImplementedError
 
 
 class batch():
     """docstring for batch."""
 
-    def __init__(self, arg):
+    def __init__(self, data, type):
         super(batch, self).__init__()
-        self.arg = arg
-    x = []
-    y = []
+        self.data = data
+        self.type = type
+        x = self.data.data
+        y = self.data.lables
 
-    def split(data, type):
+
+    def split(data, lables, percentData, type):
+
+
+        training = data * percentData
+
         raise NotImplementedError
 
-def load(data, numbbaches): # split data into batches
+def load(data, lables, numbbaches): # split data into batches
     type = data.type;
     dataLength = len(data)
     segmentSize = datalength/numbacches
